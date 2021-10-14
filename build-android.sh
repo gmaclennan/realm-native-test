d6871cfd1dd9c35adf86894c6d2073d0e0606522
Gregor MacLennan <gmaclennan@digital-democracy.org>
2021-10-14 23:43:45 +0100

    fix: use ANDROID_SDK_ROOT env which is defined in Github action env

--- build-android.sh
+++ build-android.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-export CMAKE_JS_ARGS="-r node -v 12.16.3 -a arm64 -l verbose -G Ninja --CDANDROID_ABI=arm64-v8a --CDANDROID_PLATFORM=21 --CDCMAKE_SYSTEM_NAME=Android --CDCMAKE_MAKE_PROGRAM=${ANDROID_SDK}/cmake/3.18.1/bin/ninja --CDCMAKE_TOOLCHAIN_FILE=${ANDROID_SDK}/ndk/21.4.7075529/build/cmake/android.toolchain.cmake --CDCMAKE_CXX_FLAGS='-DBUILDING_NODE_EXTENSION' --CDCMAKE_JS_INC=${PWD}/node_modules/nodejs-mobile-react-native/android/libnode/include/node"
+export CMAKE_JS_ARGS="-r node -v 12.16.3 -a arm64 -l verbose -G Ninja --CDANDROID_ABI=arm64-v8a --CDANDROID_PLATFORM=21 --CDCMAKE_SYSTEM_NAME=Android --CDCMAKE_MAKE_PROGRAM=${ANDROID_SDK_ROOT}/cmake/3.18.1/bin/ninja --CDCMAKE_TOOLCHAIN_FILE=${ANDROID_SDK_ROOT}/ndk/21.4.7075529/build/cmake/android.toolchain.cmake --CDCMAKE_CXX_FLAGS='-DBUILDING_NODE_EXTENSION' --CDCMAKE_JS_INC=${PWD}/node_modules/nodejs-mobile-react-native/android/libnode/include/node"
 
 echo $CMAKE_JS_ARGS
 npm rebuild --build-from-source
